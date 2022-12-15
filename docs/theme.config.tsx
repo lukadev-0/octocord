5d62380938d7616328ca012721862e7ad5f3f284
Luka <47296785+lukadev-0@users.noreply.github.com>
2022-12-15 16:01:23 +0100

    docs: :memo: add favicon

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -82,6 +82,7 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.svg" />
     </>
   ),
   banner: {
